689fdead9e0d6b0ab03de2d85a31635a67c1d4a8
Geraldy <geraldy.leondas@gmail.com>
2018-01-12 23:58:58 +0100

    mysql

---
+++ cour 14 - MySQL/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP et MySQL</title>
+    <link rel="stylesheet" href="../style.css">
+    <script src="https://use.fontawesome.com/40a1716643.js"></script>
+</head>
+<body>
+   <div class="content-mysql">
+       <h1>Concevez votre site web avec <br> PHP and MySQL</h1>
+       <h2>Les bases de données</h2>
+   </div>
+   <div>
+       <h2>Structure d'une base de données</h2>
+       <p>Avec les bases de données, il faut utiliser un vocabulaire précis. Heureusement, vous ne devriez pas avoir trop de mal à vous en souvenir, vu qu'on va se servir d'une image : celle d'une armoire. Écoutez-moi attentivement et n'hésitez pas à lire lentement, plusieurs fois si c'est nécessaire.</p>
+       <p>Je vous demande d'imaginer ce qui suit.</p>
+       <ul>
+           <li>L'armoire est appelée la base dans le langage SQL. C'est le gros meuble dans lequel les secrétaires ont l'habitude de classer les informations.</li>
+           <li>Dans une armoire, il y a plusieurs tiroirs. Un tiroir, en SQL, c'est ce qu'on appelle une table. Chaque tiroir contient des données différentes. Par exemple, on peut imaginer un tiroir qui contient les pseudonymes et infos sur vos visiteurs, un autre qui contient les messages postés sur votre forum…
+
+</li>
+           <li>Mais que contient une table ? C'est là que sont enregistrées les données, sous la forme d'un tableau. Dans ce tableau, les colonnes sont appelées <span>des champs</span>, et les lignes sont appelées <span>des entrées.</span></li>
+       </ul>
+   </div>
+   
+    
+</body>
+</html>
